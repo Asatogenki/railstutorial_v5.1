2e122e96b05a320e35d9c942ada4819ac3671008
Asatogenki <asa.326.gen@gmail.com>
2020-06-01 22:41:34 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  # test "should get root" do
+  #   get FILL_IN
+  #   assert_response FILL_IN
+  # end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -14,5 +20,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-  end 
+  end
 end
