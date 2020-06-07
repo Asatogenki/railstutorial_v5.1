cdae288063e4a81f21358ee46ac2e5253c8f11f9
Asatogenki <asa.326.gen@gmail.com>
2020-06-07 18:17:30 +0900

    コンタクト問題かいけつ！

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -27,7 +27,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contaczt | #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
