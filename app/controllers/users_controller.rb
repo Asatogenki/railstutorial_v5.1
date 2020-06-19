c64debf5bff47f423187d7b012708f433fab0cd0
Asatogenki <asa.326.gen@gmail.com>
2020-06-19 23:24:14 +0900

    中間プッシュ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user.params)
+      # 更新に成功するとここ
+    else
+      render 'new'
+    end
+  end
   private
 
    def user_params
