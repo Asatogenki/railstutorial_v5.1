0d473a9c63338158d78bc8c190e614548374b73d
Asatogenki <asa.326.gen@gmail.com>
2020-06-20 14:28:08 +0900

    ブランチ切替

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user.params)
-      # 更新に成功するとここ
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
   end
   private
